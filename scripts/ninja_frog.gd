c993d2ba1060f02bb0d898ff3f87246ebdde1cb1
chukmunnlee <chukmunnlee@gmail.com>
2024-01-20 15:02:13 +0800

    move ninja frog

---
+++ scripts/ninja_frog.gd
@@ -0,0 +1,43 @@
+class_name NinjaFrog
+extends CharacterBody2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@export var SPEED: float = 150
+@export var JUMP: float = 250
+#@export var GRAVITY: float = 300.0
+var GRAVITY: float = ProjectSettings.get("physics/2d/default_gravity")
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta):
+	var dir
+	var anim = "idle"
+	
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		#anim = "jump" if velocity.y < 0 else "fall"
+		if velocity.y > 0:
+			anim = "fall"
+		else:
+			anim = "jump"
+		
+	else:
+		dir = Input.get_axis("left", "right")
+		velocity.x = SPEED * dir
+		#anim = "idle" if dir == 0 else "run"
+		if dir == 0:
+			anim = "idle"
+		else:
+			anim = "run"
+			animated_sprite_2d.flip_h = dir < 0
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMP
+		
+	velocity.y = min(velocity.y, GRAVITY)
+	
+	move_and_slide()
+	
+	animated_sprite_2d.play(anim)
+		
